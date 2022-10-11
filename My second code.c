9aacb5fbf9c37e53d2012f80e9203123d64df1f3
Wontiger <624tiger@naver.com>
2022-10-11 22:48:58 +0900

    Hello&Bye

---
+++ My second code.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		printf("안녕히가세요!\n");
+	}
+	return 0;
+}
---
+++ My thrid code.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		printf("¾È³çÇÏ¼¼¿ä!\n");
+	}
+	return 0;
+}
